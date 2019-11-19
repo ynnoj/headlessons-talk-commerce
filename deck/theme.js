af6e98cc706a1c952a0787affbcdcd3aef84792c
Jonathan Steele <jonathan@steele.pro>
2019-11-19 11:22:19 +0100

    feat: Add base deck

---
+++ deck/theme.js
@@ -0,0 +1,27 @@
+import { themes } from 'mdx-deck'
+
+export default {
+  ...themes.future,
+  styles: {
+    img: {
+      width: '40%',
+      margin: '0 auto'
+    },
+    h1: {
+      lineHeight: '1',
+      margin: '0 0 1rem'
+    },
+    h2: {
+      color: 'white'
+    },
+    p: {
+      color: '#999',
+      margin: '0'
+    }
+  },
+  colors: {
+    background: '#161924',
+    code: '#FB1488',
+    text: 'white'
+  }
+}
